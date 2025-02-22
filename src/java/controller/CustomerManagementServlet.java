8c1cb012c7cb352f42a531da5215a16534f75cc8
VoDaiVy <vyvdde180817@fpt.edu.vn>
2025-02-22 16:09:18 +0700

    Customer Management Servlet

---
+++ src/java/controller/CustomerManagementServlet.java
@@ -0,0 +1,72 @@
+package controller;
+
+import dal.PersonDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.sql.Date;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Person;
+
+/**
+ *
+ * @author admin
+ */
+@WebServlet(name = "CustomerController", urlPatterns = {"/customer-management"})
+public class CustomerManagementServlet extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PersonDAO personDAO = new PersonDAO();
+
+        List<Person> customerList = personDAO.getPersonByRole("customer");
+        System.out.println(customerList.get(0).getId());
+        request.setAttribute("customerList", customerList);
+        request.getRequestDispatcher("customer-management.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       
+//         Xử lý cập nhật thông tin khách hàng
+        String idStr = request.getParameter("id");
+        String name = request.getParameter("name");
+        String dateOfBirthStr = request.getParameter("dateOfBirth");
+        String genderStr = request.getParameter("gender");
+        String phone = request.getParameter("phone");
+        String email = request.getParameter("email");
+        String address = request.getParameter("address");
+
+        // Kiểm tra các trường nhập trống
+        if (idStr == null || idStr.trim().isEmpty()
+                || name == null || name.trim().isEmpty()
+                || dateOfBirthStr == null || dateOfBirthStr.trim().isEmpty()
+                || genderStr == null || genderStr.trim().isEmpty()
+                || phone == null || phone.trim().isEmpty()
+                || email == null || email.trim().isEmpty()
+                || address == null || address.trim().isEmpty()) {
+
+            request.setAttribute("errorMessage", "Vui lòng điền đầy đủ tất cả các trường.");
+            request.getRequestDispatcher("customer-management.jsp").forward(request, response);
+            return;
+        }
+
+        // Chuyển đổi dateOfBirth từ chuỗi sang java.sql.Date
+
+
+}
