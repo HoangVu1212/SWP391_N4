b231a83a60d09a0c56bff717d2b5f42924096c25
TrinhMinhHai180679 <haitmde180679@fpt.edu.vn>
2025-02-22 13:04:40 +0700

    bookingsevlet

---
+++ src/java/controller.Booking/BookingServlet.java
@@ -0,0 +1,36 @@
+package controller.Booking;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class BookingServlet extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet BookingServlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet BookingServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "BookingServlet handles booking appointments.";
+    }
+}
