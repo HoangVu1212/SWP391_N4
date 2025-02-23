94f1dd5a3c5996a74064f987a100a3c421de86fd
TrinhMinhHai180679 <haitmde180679@fpt.edu.vn>
2025-02-23 16:58:12 +0700

    time

--- src/java/controller.Booking/GetAvailableTimes.java
+++ src/java/controller.Booking/GetAvailableTimes.java
@@ -4,28 +4,35 @@
  */
 package controller.Booking;
 
+import dal.AppointmentDAO;
+import dal.AppointmentServiceDAO;
+import dal.PersonDAO;
+import dal.ServiceDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
+import jakarta.servlet.http.HttpSession;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Account;
+import model.Appointment;
+import model.Person;
+import model.Service;
 
 /**
  *
  * @author ADMIN
  */
-public class GetAvailableTimes extends HttpServlet {
+public class BookingServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
@@ -33,52 +40,88 @@ public class GetAvailableTimes extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet GetAvailableTimes</title>");
+            out.println("<title>Servlet BookingServlet</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet GetAvailableTimes at " + request.getContextPath() + "</h1>");
+            out.println("<h1>Servlet BookingServlet at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            ServiceDAO serviceDAO = new ServiceDAO();
+            PersonDAO personDAO = new PersonDAO();
+            List<Service> serviceList = serviceDAO.selectAllServices();
+            List<Person> staffList = personDAO.getPersonByRole("Staff");
+            request.setAttribute("staffList", staffList);
+            request.setAttribute("serviceList", serviceList);
+            request.setAttribute("today", LocalDate.now());
+            request.getRequestDispatcher("booking.jsp").forward(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(getClass().getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            AppointmentDAO appointmentDAO = new AppointmentDAO();
+            ServiceDAO serviceDAO = new ServiceDAO();
+            HttpSession session = request.getSession(false);
+            if (session == null || session.getAttribute("account") == null) {
+                response.sendRedirect("login");
+                return;
+            }
+
+            Account account = (Account) session.getAttribute("account");
+            String[] serviceIds = request.getParameter("selectedServices").split(",");
+            int totalDuration = totalDuration(serviceIds);
+            LocalDate date = LocalDate.parse(request.getParameter("appointmentDate"));
+            LocalTime time = LocalTime.parse(request.getParameter("selectedTime"));
+            LocalDateTime start = LocalDateTime.of(date, time);
+            String note = request.getParameter("notes");
+            Appointment appointment = new Appointment();
+            appointment.setStart(start);
+            appointment.setEnd(start.plusMinutes(totalDuration));
+            appointment.setCreatedDate(LocalDateTime.now());
+            appointment.setStatus("Scheduled");
+            appointment.setNote(note);
+            appointment.setCustomer(account.getPersonInfo());
+            if (appointmentDAO.addAppointment(appointment)) {
+                for (String id : serviceIds) {
+                    int serviceID = Integer.parseInt(id);
+                    Service service = serviceDAO.selectService(serviceID);
+                    int staffId = Integer.parseInt(request.getParameter("staff"));
+                    AppointmentServiceDAO serviceList = new AppointmentServiceDAO();
+                    serviceList.addAppointmentService(appointmentDAO.getMaxAppointmentID(), service.getId(), staffId);
+                }
+            }
+            request.setAttribute("success", "Booking successfully!");
+            doGet(request, response);
+        } catch (SQLException ex) {
+            Logger.getLogger(BookingServlet.class.getName()).log(Level.SEVERE, null, ex);
+            response.sendRedirect("error");
+        }
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
     @Override
     public String getServletInfo() {
         return "Short description";
-    }// </editor-fold>
+    }
 
+    private int totalDuration(String[] serviceIds) throws SQLException {
+        ServiceDAO serviceDAO = new ServiceDAO();
+        int totalDuration = 0;
+        for (String serviceIdStr : serviceIds) {
+            int serviceID = Integer.parseInt(serviceIdStr);
+            Service service = serviceDAO.selectService(serviceID);
+            totalDuration += service.getDuration();
+        }
+        return totalDuration;
+    }
 }
